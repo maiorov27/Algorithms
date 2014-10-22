1ea438f84f75dffecfd62f2edb2a650d3408227c
Egor Maiorov <maiorov.prog@gmail.com>
2014-10-22 22:52:12 +0300

    some tests added

--- Randomized Queues and Deques/src/DequeTest.java
+++ Randomized Queues and Deques/src/DequeTest.java
@@ -1,5 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -17,9 +18,37 @@ public class DequeTest {
         deque.addFirst(1);
         deque.addFirst(2);
         deque.addFirst(3);
-        assertThat(deque.removeFirst()).isEqualTo(1);
-        assertThat(deque.removeFirst()).isEqualTo(2);
         assertThat(deque.removeFirst()).isEqualTo(3);
+        assertThat(deque.removeFirst()).isEqualTo(2);
+        assertThat(deque.removeFirst()).isEqualTo(1);
     }
+
+    @Test
+    public void addElementToTheEndOfTheDeque() {
+        deque.addLast(1);
+        deque.addLast(2);
+        deque.addLast(3);
+        assertThat(deque.removeLast()).isEqualTo(3);
+        assertThat(deque.removeLast()).isEqualTo(2);
+        assertThat(deque.removeLast()).isEqualTo(1);
+    }
+
+    @Test
+    public void addElementAsFirstAndThenReturnItOnGetLastRequest() {
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addLast(3);
+        deque.addFirst(4);
+        assertThat(deque.removeLast()).isEqualTo(3);
+        assertThat(deque.removeFirst()).isEqualTo(4);
+        assertThat(deque.removeLast()).isEqualTo(1);
+    }
+
+    @Test
+    public void addElementAsLastAndThenReturnItOnGetFirstRequest() {
+        deque.addLast(1);
+        assertThat(deque.removeFirst()).isEqualTo(1);
+    }
+
 
 }
