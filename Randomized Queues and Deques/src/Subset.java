9ba83b59f3a565116a0371a16ce235bccbfb61d7
Egor Maiorov <maiorov.prog@gmail.com>
2014-10-23 21:59:53 +0300

    subsetAdded

--- Randomized Queues and Deques/src/Subset.java
+++ Randomized Queues and Deques/src/Subset.java
@@ -2,7 +2,17 @@
 public class Subset {
 
     public static void main(String[] args) {
+        RandomizedQueue randQueue = new RandomizedQueue();
 
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            randQueue.enqueue(s);
+        }
+
+        Integer amount =  Integer.parseInt(args[0]);
+        for(int i = 0; i < amount; i++) {
+            StdOut.println(randQueue.dequeue());
+        }
     }
 
 }
